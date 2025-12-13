3b818a2e8a2bc4f3883e1ebc478365f3c6a3a24a
ahmedmfekry <33072642+ahmedmfekry@users.noreply.github.com>
2025-12-13 21:26:52 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 task<Delete>("clean") {
