00a7731e0c44fdcd970ded89f4ea7a1366b0feed
ahmedmfekry <33072642+ahmedmfekry@users.noreply.github.com>
2025-12-14 13:43:41 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("com.google.gms.google-services")
     id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
 }
 
 android {
